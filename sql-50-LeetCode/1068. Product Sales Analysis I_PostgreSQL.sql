b2e21980483fc5817b5afddc986d8e7a11de40e3
ahad11621 <ahad11621@gmail.com>
2024-02-04 15:32:57 +0300

    Solution-PostgreSQL_1068. Product Sales Analysis I

---
+++ sql-50-LeetCode/1068. Product Sales Analysis I_PostgreSQL.sql
@@ -0,0 +1,10 @@
+/*
+Problem link:
+https://leetcode.com/problems/product-sales-analysis-i/
+*/
+
+--Solution:
+SELECT p.product_name, s.year, s.price
+FROM Sales s
+LEFT JOIN Product p
+ON s.product_id = p.product_id
