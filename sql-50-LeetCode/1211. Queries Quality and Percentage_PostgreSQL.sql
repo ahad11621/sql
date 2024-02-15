38899d9a47a42647148086a59c68429abccbea96
ahad11621 <ahad11621@gmail.com>
2024-02-15 15:13:08 +0300

    Solution-PostgreSQL_1211. Queries Quality and Percentage

---
+++ sql-50-LeetCode/1211. Queries Quality and Percentage_PostgreSQL.sql
@@ -0,0 +1,10 @@
+/*
+Problem link:
+https://leetcode.com/problems/queries-quality-and-percentage/
+*/
+
+--Solution:
+SELECT query_name, ROUND(AVG(rating * 1.00/ position),2) AS quality, ROUND(AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END)*100.00, 2) AS poor_query_percentage
+FROM Queries
+WHERE query_name IS NOT NULL
+GROUP BY query_name
