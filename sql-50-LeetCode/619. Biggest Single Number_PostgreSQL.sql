59a2cee4bcb27e5f0856db92b7efb2b68f69022a
ahad11621 <ahad11621@gmail.com>
2024-02-19 10:15:17 +0300

    Solution-PostgreSQL_619. Biggest Single Number

---
+++ sql-50-LeetCode/619. Biggest Single Number_PostgreSQL.sql
@@ -0,0 +1,13 @@
+/*
+Problem link:
+https://leetcode.com/problems/biggest-single-number/
+*/
+
+--Solution:
+SELECT MAX(num) AS num
+FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+)
