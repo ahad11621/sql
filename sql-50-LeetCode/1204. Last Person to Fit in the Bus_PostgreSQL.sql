17025e456027937a527fc1866c369b55d2da5a17
ahad11621 <ahad11621@gmail.com>
2024-02-21 12:13:01 +0300

    Solution-PostgreSQL_1204. Last Person to Fit in the Bus

---
+++ sql-50-LeetCode/1204. Last Person to Fit in the Bus_PostgreSQL.sql
@@ -0,0 +1,16 @@
+/*
+Problem link:
+https://leetcode.com/problems/last-person-to-fit-in-the-bus/
+*/
+
+--Solution:
+WITH CTE AS
+(
+    SELECT person_name, SUM(weight) OVER(ORDER BY turn) AS total_weight 
+    FROM Queue
+)
+SELECT person_name
+FROM CTE
+WHERE total_weight <= 1000
+ORDER BY total_weight DESC
+LIMIT 1
