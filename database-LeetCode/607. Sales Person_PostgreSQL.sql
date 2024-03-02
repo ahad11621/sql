3ce929c78c4f781e909e28537387db2c4d06c434
ahad11621 <ahad11621@gmail.com>
2024-03-02 14:33:34 +0300

    Solution-PostgreSQL_607. Sales Person

---
+++ database-LeetCode/607. Sales Person_PostgreSQL.sql
@@ -0,0 +1,15 @@
+/*
+Problem link:
+https://leetcode.com/problems/sales-person/
+*/
+
+--Solution:
+SELECT name
+FROM SalesPerson
+WHERE sales_id NOT IN (
+    SELECT o.sales_id
+    FROM Orders o
+    JOIN Company c
+    ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+)
