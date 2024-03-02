d24ec83ff195044f38e4c05df5406867ab8a9129
ahad11621 <ahad11621@gmail.com>
2024-03-02 15:04:38 +0300

    Solution-PostgreSQL_1050. Actors and Directors Who Cooperated At Least Three Times

---
+++ database-LeetCode/1050. Actors and Directors Who Cooperated At Least Three Times_PostgreSQL.sql
@@ -0,0 +1,10 @@
+/*
+Problem link:
+https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/
+*/
+
+--Solution:
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(timestamp) > 2
