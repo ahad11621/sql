c6a1b46c8884b394f0cb6dd639e7d884c18ee2b1
ahad11621 <ahad11621@gmail.com>
2024-03-03 16:08:18 +0300

    Solution-PostgreSQL_1693. Daily Leads and Partners

---
+++ database-LeetCode/1693. Daily Leads and Partners_PostgreSQL.sql
@@ -0,0 +1,9 @@
+/*
+Problem link:
+https://leetcode.com/problems/daily-leads-and-partners/
+*/
+
+--Solution:
+SELECT date_id, make_name, COUNT(DISTINCT lead_id) AS unique_leads, COUNT(DISTINCT partner_id) AS unique_partners
+FROM DailySales
+GROUP BY date_id, make_name
