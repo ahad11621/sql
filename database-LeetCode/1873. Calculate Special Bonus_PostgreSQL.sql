8ad31dc57d817b50d850e729f3366de4df79131d
ahad11621 <ahad11621@gmail.com>
2024-03-03 16:42:35 +0300

    Solution-PostgreSQL_1873. Calculate Special Bonus

---
+++ database-LeetCode/1873. Calculate Special Bonus_PostgreSQL.sql
@@ -0,0 +1,13 @@
+/*
+Problem link:
+https://leetcode.com/problems/calculate-special-bonus/
+*/
+
+--Solution:
+SELECT employee_id,
+CASE
+    WHEN employee_id % 2 = 1 AND LEFT(name, 1) <> 'M' THEN salary
+    ELSE 0
+END AS bonus
+FROM Employees
+ORDER BY employee_id
