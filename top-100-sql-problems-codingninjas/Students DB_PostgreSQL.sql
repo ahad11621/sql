8fc21f1d49e7e119d43fcacba114738be88deaac
ahad11621 <ahad11621@gmail.com>
2024-03-10 14:29:13 +0300

    Solution-PostgreSQL_Students DB

---
+++ top-100-sql-problems-codingninjas/Students DB_PostgreSQL.sql
@@ -0,0 +1,10 @@
+/*
+Problem link:
+https://www.codingninjas.com/studio/problems/students-data_2105460
+*/
+
+--Solution:
+INSERT INTO students (id, name, gender)
+VALUES (3, 'Kim', 'F'), (4, 'Molina', 'F'), (5, 'Dev', 'M');
+
+SELECT * FROM students;
