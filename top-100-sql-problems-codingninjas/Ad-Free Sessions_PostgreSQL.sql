ff564542e62e48ec36635ef8a4a566d224d981c9
ahad11621 <ahad11621@gmail.com>
2024-03-16 13:38:46 +0300

    Solution-PostgreSQL_Ad-Free Sessions

---
+++ top-100-sql-problems-codingninjas/Ad-Free Sessions_PostgreSQL.sql
@@ -0,0 +1,18 @@
+/*
+Problem link:
+https://www.codingninjas.com/studio/problems/ad-free-sessions_2191517
+*/
+
+--Solution:
+SELECT DISTINCT session_id
+FROM (
+    SELECT p.session_id, p.start_time, p.end_time, a.timestamp,
+    CASE
+        WHEN timestamp >= start_time AND timestamp <= end_time THEN 'Yes'
+        ELSE 'No' 
+    END AS ads
+    FROM Playback p
+    LEFT JOIN Ads a
+    ON p.customer_id = a.customer_id
+) AS s 
+WHERE s.ads = 'No'
